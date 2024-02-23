4a2ead97f2ad75f4e6e7322d0357e3239d6383e1
kon1ac <isip_i.v.egorov@mpt.ru>
2024-02-23 20:22:35 +0300

    first commit

---
+++ MagicPract3/MagicPract3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	setlocale(0, "Russian");
+    const int rows = 3;
+    const int cols = 5;
+    float array[rows][cols];
+
+    // Ввод элементов массива с клавиатуры
+    for (int i = 0; i < rows; i++) {
+        std::cout << "Введите элементы " << i + 1 << "-й строки:";
+        for (int j = 0; j < cols; j++) {
+            std::cin >> array[i][j];
+        }
+    }
+
+    // Вычисление и вывод среднего арифметического для каждой строки
+    for (int i = 0; i < rows; i++) {
+        float sum = 0;
+        for (int j = 0; j < cols; j++) {
+            sum += array[i][j];
+        }
+        float average = sum / cols;
+        std::cout << "Среднее арифметическое элементов " << i + 1 << "-й строки: " << average << std::endl;
+    }
+
+    return 0;
+}
+
